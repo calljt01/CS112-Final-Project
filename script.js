67ad0b6f63068df383215b21027d0db5f25c0070
Justin Call <jtcall0121@gmail.com>
2016-11-29 00:10:59 -0500

    Completed and tested and debugged the main game.

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
  * Created by Justin Call on 10/30/2016.
  */
 
-moneyList = [.01,1,5,10,25,50,75,100,200,300,400,500,750,1000,5000,10000,25000,50000,75000,100000,200000,300000,400000,500000,750000,1000000];
+var moneyList = [.01,1,5,10,25,50,75,100,200,300,400,500,750,1000,5000,10000,25000,50000,75000,100000,200000,300000,400000,500000,750000,1000000];
 var sessionList = [];
+var primaryCase;
+var primaryMoney;
+var suitcasesLeft;
+var deal;
 function Get(element){
     return document.getElementById(element);
 }
@@ -11,13 +15,13 @@ function RandInt(low, high){
     return Math.floor(Math.random()*(high-low+1) + low);
 }
 function Fade(element){
-    document.getElementById(element).setAttribute("class", "faded");
+    Get(element).setAttribute("class", "faded");
 }
 function Disable(element){
-    document.getElementById(element).setAttribute("disabled", "true");
+    Get(element).setAttribute("disabled", "true");
 }
 function Enable(element){
-    document.getElementById(element).removeAttribute("disabled");
+    Get(element).removeAttribute("disabled");
 }
 function GetSuitcase(){
     var suitcase;
@@ -79,11 +83,76 @@ function GetSuitcase(){
 function Shuffle(list){
     var mixList = list;
     var temporary;
-    for (var i = 1; i<mixList.length; i++){
-        j = RandInt(0,26);
+    for (var i = 0; i<mixList.length; i++){
+        j = RandInt(0,25);
         temporary = mixList[i];
         mixList[i] = mixList[j];
         mixList[j] = temporary;
     }
+    moneyList = [.01,1,5,10,25,50,75,100,200,300,400,500,750,1000,5000,10000,25000,50000,75000,100000,200000,300000,400000,500000,750000,1000000];
     return mixList
+}
+function Load(){
+    sessionList = Shuffle(moneyList);
+    suitcasesLeft = 26;
+    Get("divUserPrompt").innerHTML = "Choose your case.";
+}
+function findIndex(money){
+    for (var i = 0; i < sessionList.length; i++){
+        if (moneyList[i] == money){
+            return i;
+        }
+    }
+}
+function Play(){
+    var suitcase = GetSuitcase();
+    var money = sessionList[suitcase-1];
+    sessionList[suitcase-1] = 0;
+    var element = "suitcase" + suitcase;
+    Disable(element);
+    suitcasesLeft -= 1;
+    var index = findIndex(money);
+    element = "" + index;
+    Fade(element);
+    Get("divUserPrompt").innerHTML = "Case #" + suitcase + " held $" + money + ". Choose from the remaining cases." + suitcasesLeft;
+    if (suitcasesLeft == 1){
+        Disable("btnEnter");
+        Get("divUserPrompt").innerHTML = "Your suitcase is the only one left. You win $" + primaryMoney + "!!!";
+    }
+    if (suitcasesLeft < 13 && suitcasesLeft%2==0){
+        Deal();
+    }
+}
+function ChooseCase(){
+    primaryCase = GetSuitcase();
+    primaryMoney = sessionList[primaryCase - 1];
+    var Element = "suitcase" + primaryCase;
+    Disable(Element);
+    Disable("btnChooseCase");
+    Enable("btnEnter");
+    Get("divCaseNumber").innerHTML = primaryCase;
+    Get("divUserPrompt").innerHTML = "You have chosen case #" + primaryCase + ". Choose from the remaining cases."
+}
+function Deal(){
+    Enable("btnAccept");
+    Enable("btnDecline");
+    Disable("btnEnter");
+    var total = 0;
+    for (i = 0; i < sessionList.length; i++){
+        total += sessionList[i];
+    }
+    deal = (total/suitcasesLeft).toFixed(2);
+    Get("divDealerPrompt").innerHTML = "The banker has given you an offer of $" + deal + " Do you accept?";
+}
+function Accept(){
+    Disable("btnAccept");
+    Disable("btnDecline");
+    Get("divUserPrompt").innerHTML = "You have accepted the offer! You win $" + deal + "!!!";
+    Get("divDealerPrompt").innerHTML = "Your case held $" + primaryMoney + ".";
+}
+function Decline(){
+    Disable("btnAccept");
+    Disable("btnDecline");
+    Enable("btnEnter")
+    Get("divDealerPrompt").innerHTML = "";
 }
